ede1e5fbcaa6843fdbc5fa60e656281a185ba1f9
Amir Torres <johnamtorres@gmail.com>
2015-09-27 15:05:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
-var apptitle = 'Horario de Clase 5to A sistemas';
-var actualversion = '0.5.5';
+{
+	"name": "Horario 5to A",
+	"version": "0.5.5",
+	"description": "Horario escolar de el campus ITSC",
+	"launch_path": "/index.html",
+	"default_locale": "es",
+	"size": 103973,
+	"icons": {
+		"128": "/src/icons/128.png"
+	},
+	"developer": {
+		"name": "@amirtorrez",
+		"url": "http://twitter.com/amirtorrez"
+	},
+	"school":{
+		"grade": "5to Semestre",
+		"group": "A",
+		"turn": "Matutino",
+		"classroom": "G3"
+	},
+	"installs_allowed_from": ["*"],
+	"orientation": ["portrait-primary"]
+}
